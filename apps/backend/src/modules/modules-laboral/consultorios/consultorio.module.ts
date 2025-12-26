02de878cd01eb0ce28d65e31732bf5da158e60d8
Federico Ramirez <ramirezfede37@gmail.com>
2025-12-26 17:46:41 -0300

    commit

--- apps/backend/src/modules/modules-laboral/consultorios/consultorio.module.ts
+++ apps/backend/src/modules/modules-laboral/consultorios/consultorio.module.ts
@@ -3,7 +3,13 @@ import { Module } from '@nestjs/common';
 import { ConsultoriosController } from './consultorio.controller';
 import { ConsultoriosService } from './consultorios.service';
 
+// IMPORTANTE: Asegúrate de que la ruta sea correcta hacia tu AuthModule
+import { AuthModule } from '../../auth/auth.module'; 
+
 @Module({
+  imports: [
+    AuthModule // <--- ESTO ES LO QUE FALTA. Al importar AuthModule, traes el JwtService y el Guard.
+  ],
   controllers: [ConsultoriosController],
   providers: [ConsultoriosService],
 })
